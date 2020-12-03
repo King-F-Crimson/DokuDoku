8702f45004c28a57acc9a5fef0ff775a78e97df2
tom <>
2020-12-03 20:45:26 +0700

    Make blocks placeable (without check)

--- __main__.py
+++ __main__.py
@@ -10,7 +10,9 @@ board = [[False for i in range(board_size)] for j in range(board_size)]
 screen_width = block_size * (board_size + 2 + 5)
 screen_height = block_size * (board_size + 2 + 5)
 
+background_color = pygame.Color(0, 0, 0)
 block_color = pygame.Color(0, 200, 240)
+transparent_block_color = pygame.Color(0, 50, 60)
 border_color = pygame.Color(240, 240, 240)
 
 shape_list = [
@@ -57,18 +59,64 @@ for i in range(shape_selection_count):
 
 selected_shape = None
 
-def draw_shape(shape, left, top):
+def draw_shape(shape, left, top, color):
     for row in range(4):
         for col in range(4):
             if shape[row][col]:
-                pygame.draw.rect(screen, block_color, pygame.Rect(left + col * block_size, top + row * block_size, block_size, block_size))
+                pygame.draw.rect(screen, color, pygame.Rect(left + col * block_size, top + row * block_size, block_size, block_size))
                 pygame.draw.rect(screen, border_color, pygame.Rect(left + col * block_size, top + row * block_size, block_size, block_size), 1)
+
+def select_shape(event):
+    x, y = event.pos
+    block_x = x // block_size
+    block_y = y // block_size
+
+    # Check if cursor y is out of range
+    if block_y <= board_size + 1 or block_y >= board_size + 5:
+        return
+
+    # Check which shape is selected by X
+    global selected_shape
+    if block_x >= 1 and block_x <= 5:
+        selected_shape = shape_selection[0]
+    elif block_x >= 6 and block_x <= 10:
+        selected_shape = shape_selection[1]
+    elif block_x >= 11 and block_x <= 15:
+        selected_shape = shape_selection[2]
+
+def place_shape(event):
+    x, y = event.pos
+    block_x = x // block_size
+    block_y = y // block_size
+
+    global selected_shape
+    for row in range(4):
+        for col in range(4):
+            if selected_shape[row][col]:
+                board[block_y - 1 + row][block_x - 1 + col] = True
+
+    selected_shape = None
+
+def is_placeable():
+    x, y = pygame.mouse.get_pos()
+    block_x = x // block_size
+    block_y = y // block_size
+
+    return True
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if selected_shape == None:
+                    select_shape(event)
+                elif is_placeable():
+                    place_shape(event)
+
+    screen.fill(background_color)
 
     # Draw blocks
     for row in range(board_size):
@@ -79,6 +127,18 @@ while running:
 
     # Draw shape selection
     for i in range(shape_selection_count):
-        draw_shape(shape_selection[i], (i * 5 + 1) * block_size, (board_size + 2) * block_size)
+        draw_shape(shape_selection[i], (i * 5 + 1) * block_size, (board_size + 2) * block_size, block_color)
+
+    # Draw selected shape
+    if selected_shape != None:
+        x, y = pygame.mouse.get_pos()
+        block_x = x // block_size
+        block_y = y // block_size
+
+        # Draw placement guide
+        draw_shape(selected_shape, block_x * block_size, block_y * block_size, transparent_block_color)
+
+        # Draw selected shape on cursor
+        draw_shape(selected_shape, x - (block_size / 2), y - (block_size / 2), block_color)
 
     pygame.display.flip()
