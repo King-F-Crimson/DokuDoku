52107712d5a75fb311fcac12a958752c52fd6683
tom <>
2020-12-18 23:00:18 +0700

    Make non-functional DQL model used.

--- agent.py
+++ agent.py
@@ -1,13 +1,55 @@
 import random
 
 from game import Game
+from build_model import build_model
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+import numpy as np
+
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 class Agent:
     def __init__(self, game):
         self.game = game
-        self.action_index = 0
+
+        self.input_size = (game.board_size ** 2) + ((game.shape_size ** 2) * (game.shape_selection_count + 1))
+        self.output_size = (game.board_size ** 2) + game.shape_selection_count
+
+        self.model = build_model(self.input_size, self.output_size)
 
     def get_state(self):
+        state = []
+
+        # 0-99: board
+        for row in self.game.board:
+            for block in row:
+                state.append(block)
+
+        # 100-174: shapes, 25 for each shape
+        for shape in self.game.shape_selection:
+            for row in shape:
+                for block in row:
+                    state.append(block)
+        # Fill empty shape slots with False
+        state += [False] * (175 - len(state))
+
+        # 175-199: selected shape if any
+        if self.game.selected_shape != None:
+            for row in self.game.selected_shape:
+                for block in row:
+                    state.append(block)
+        else:
+            # If none is selected fill with False
+            state += [False] * 25
+
+        return np.array(state).reshape(-1, self.input_size)
+
+    def get_q_table(self):
         q_table = [0] * 103
         if self.game.selected_shape == None:
             for i in range(len(self.game.shape_selection)):
@@ -22,7 +64,11 @@ class Agent:
         return q_table
 
     def get_action(self):
-        q_table = self.get_state()
+        action = np.argmax(self.model.predict(self.get_state()))
+        print(action)
+        return action
+
+        q_table = self.get_q_table()
         try:
             return q_table.index(1)
         except ValueError:
--- build_model.py
+++ build_model.py
@@ -2,21 +2,13 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-board_size = 10
-shape_size = 5
-shape_count = 3
+def build_model(input_size, output_size):
+    model = keras.Sequential()
+    model.add(layers.Dense(input_size, input_dim=input_size, activation='relu'))
+    model.add(layers.Dense(64, activation="relu", name="dense_1"))
+    model.add(layers.Dense(64, activation="relu", name="dense_2"))
+    model.add(layers.Dense(output_size, activation="relu", name="dense_3"))
+    model.compile(optimizer="Adam", loss="mse", metrics=["mae"])
+    print(model.summary())
 
-input_size = (board_size ** 2) + ((shape_size ** 2) * shape_count)
-output_size = (board_size ** 2) + shape_count
-
-model = keras.Sequential()
-model.add(keras.Input(shape=(input_size,)))
-model.add(layers.Dense(64, activation="relu", name="dense_1"))
-model.add(layers.Dense(64, activation="relu", name="dense_2"))
-model.add(layers.Dense(output_size, activation="relu", name="dense_3"))
-
-model.compile(loss='mse', optimizer=keras.optimizers.RMSprop())
-
-model.summary()
-
-# model.save("model")
+    return model
